9c45d7320782b0da370fddd3d992cf3b6a573730
brunosz19 <hesz.bruno05@gmail.com>
2023-05-19 01:08:12 -0500

    ADD: mobile menu to make the page be responsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,11 +3,12 @@ import Wrapper from "./Wrapper";
 import Link from "next/link";
 import Menu from "./Menu";
 
-import { GiCat } from "react-icons/gi"
-import { BsCart } from "react-icons/bs"
-import { BiMenuAltRight } from "react-icons/bi"
-import { IoMdHeartEmpty } from "react-icons/io"
-import { VscChromeClose } from "react-icons/vsc"
+import { GiCat } from "react-icons/gi";
+import { BsCart } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { VscChromeClose } from "react-icons/vsc";
+import MenuMobile from "./MenuMobile";
 
 export default function Header() {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -21,25 +22,40 @@ export default function Header() {
     >
       <Wrapper className={"h-[60px] flex justify-between items-center"}>
         <Link href="/">
-          <GiCat className="w-[40px] md:w-[60px]" size={35}/>
+          <GiCat className="w-[40px] md:w-[60px]" size={35} />
         </Link>
         <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
+        {mobileMenu && <MenuMobile showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} setMobileMenu={setMobileMenu} />}
 
         <div className="flex items-center gap-2 text-black">
           <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-            <IoMdHeartEmpty className="text-[15px] md:text-[20px]"/>
-            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">5</div>
+            <IoMdHeartEmpty className="text-[15px] md:text-[20px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              5
+            </div>
           </div>
-
 
           <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
-            <BsCart className="text-[15px] md:text-[20px]"/>
-            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">5</div>
+            <BsCart className="text-[15px] md:text-[20px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              5
+            </div>
+          </div>
+
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px]"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px]"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
           </div>
         </div>
-
-
-
       </Wrapper>
     </header>
   );
