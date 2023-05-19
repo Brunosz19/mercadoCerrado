c52a95f06b4880644557746cecc7fea4ceeac90e
brunosz19 <hesz.bruno05@gmail.com>
2023-05-19 02:10:40 -0500

    ADD: programming logic to make the header be sticky, but be hidden when you scroll enought down

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,6 +16,26 @@ export default function Header() {
   const [show, setShow] = useState("translate-y-0");
   const [lastScrollY, setLastScrollY] = useState(0);
 
+  const controlNavBar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("-translate-y-[80px]")
+      } else {
+        setShow("shadow-sm")
+      }
+    } else {
+      setShow("translate-y-0")
+    }
+    setLastScrollY(window.scrollY)
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavBar);
+    return () => {
+      window.removeEventListener("scroll", controlNavBar);
+    }
+  }, [])
+
   return (
     <header
       className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
