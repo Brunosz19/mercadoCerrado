2ba324180c1687ae1cbce8edff054bf23d0261bf
brunosz19 <hesz.bruno05@gmail.com>
2023-05-22 22:44:59 -0500

    ADD: carrousel to spin the images

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+import { BiArrowBack } from "react-icons/bi";
+
+const HeroBanner = () => {
+  return <div></div>
+}
+
+export default HeroBanner;
